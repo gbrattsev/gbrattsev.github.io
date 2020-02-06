a503cbd3cc8cbd56501699adb68941a8c1dfd941
Gennady Brattsev <56601654+gbrattsev@users.noreply.github.com>
2020-02-06 13:40:25 +0500

    Update main.js

--- slider/js/main.js
+++ slider/js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   $( "#start" ).click(function() {
     setTimeout(function() {
       $('.start , .carousel').toggleClass('hide');
-    },300);    
+    },5000);    
   });
 
 });
